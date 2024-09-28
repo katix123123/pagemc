b1a48a25516f7312ec846335b0b5163668cb96c6
katix123123 <wojteksmiech2020@gmail.com>
2024-09-28 12:36:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Gra na pagemc.pl Też wbiaj"];
+const statusMessages = ["Gra teraz na PAGEMC.PL"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
